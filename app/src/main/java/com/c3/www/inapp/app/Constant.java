ca31baf67b27e56128fc1c2d73bcc5932eccbc3e
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-07-25 16:56:08 +0545

    FROM PLURALSIGHT

--- app/src/main/java/com/c3/www/inapp/app/Constant.java
+++ app/src/main/java/com/c3/www/inapp/app/Constant.java
@@ -1,11 +1,29 @@
 package com.c3.www.inapp.app;
 
+import com.c3.www.inapp.R;
+
 /**
  * Created by Bibesh on 7/25/17.
  */
 
 public class Constant {
-    static final String TAG = "In app purchase";
-    static final String ITEM_SKU = "android.test.purchased";
-    static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx8N+nTVjCrGPeqx+cNlRZtEVx+t0CVzzsxox8De/GnF3B/56PHagxcgr/WHYM1FduhZwuiWs8TJT2rn2C3VinfyCodlvkEyODYMmLYp6L3Fylqwxo84+OUOzmMHK1NGjIM7AjOf5R39iM9B5SbiM8UZ661jN5zjOVWRKkrGKMFpqtPysQavqaZj435THJz0JEeFtjbEFQr8RwRBa4e8jEt8xQhECK2VqvkX3ODHTz+sblzMa4c4yr/NXuLpDWz62S2sUBerh7MLATekCclxtOdXNWnF1ftTJLdYqSTqtIz6m2f4whCz7hP/W8K7dMPckTDd9CTVH6wQW+gV9XCrZDwIDAQAB";
+    // Debug tag, for logging
+    public static final String TAG = "TrivialDrive";
+
+    static final String SKU_PREMIUM = "premium2";
+    static final String SKU_GAS = "gas";
+    static final String SKU_INFINITE_GAS = "infinite_gas";
+
+    static final int RC_REQUEST = 10001;
+
+
+    // Graphics for the gas gauge
+    static int[] TANK_RES_IDS = {R.drawable.gas0, R.drawable.gas1, R.drawable.gas2,
+            R.drawable.gas3, R.drawable.gas4};
+
+    // How many units (1/4 tank is our unit) fill in the tank.
+    static final int TANK_MAX = 4;
+
+    static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtTG+2TM4viC8U3WBe18YKKy3kRkosWwg1YRzvQ+El0IzyYIzyqaGpC4WmyTU0F/1RQRWx40yosZicty+W/aL6eBfjq64JKWWMLPq+yeCPdRRUcmP+nSavPVL+macLkPEvoHNd2eCvP3/ufahIG137Bp7YdSAuYokVcvdviUME2eAsHju1vUz1lTsuEfxyqY8/oxZtkOUCSJt6EqT2FfTSlczPo3m4cJ/B+kn+Mid98xHV2wwieb3RsoUwNlBJ+iUdaLTNe9K2otyBQH/j2mozbkX4s4GuFneeQ1WseHDRWLqumt8muR2oR76ZK0FJcirjTX6rUzgBLShsdeR4zXChwIDAQAB";
+
 }
