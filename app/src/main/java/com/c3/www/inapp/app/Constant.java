cef69894d53589841d70180fc752ca423fd6f32e
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-07-26 13:12:55 +0545

    FINAL BRANCH

--- app/src/main/java/com/c3/www/inapp/app/Constant.java
+++ app/src/main/java/com/c3/www/inapp/app/Constant.java
@@ -6,7 +6,10 @@ package com.c3.www.inapp.app;
 
 public class Constant {
     static final String TAG = "In app purchase";
+    static final String GAS = "gas";
+    static final String INFINITE = "infinite_gas";
+    static final String PREMIUM = "premiums";
     static final int RC_REQUEST = 10001;
     static final String ITEM_SKU = "android.test.purchased";
-    static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAx8N+nTVjCrGPeqx+cNlRZtEVx+t0CVzzsxox8De/GnF3B/56PHagxcgr/WHYM1FduhZwuiWs8TJT2rn2C3VinfyCodlvkEyODYMmLYp6L3Fylqwxo84+OUOzmMHK1NGjIM7AjOf5R39iM9B5SbiM8UZ661jN5zjOVWRKkrGKMFpqtPysQavqaZj435THJz0JEeFtjbEFQr8RwRBa4e8jEt8xQhECK2VqvkX3ODHTz+sblzMa4c4yr/NXuLpDWz62S2sUBerh7MLATekCclxtOdXNWnF1ftTJLdYqSTqtIz6m2f4whCz7hP/W8K7dMPckTDd9CTVH6wQW+gV9XCrZDwIDAQAB";
+    static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtTG+2TM4viC8U3WBe18YKKy3kRkosWwg1YRzvQ+El0IzyYIzyqaGpC4WmyTU0F/1RQRWx40yosZicty+W/aL6eBfjq64JKWWMLPq+yeCPdRRUcmP+nSavPVL+macLkPEvoHNd2eCvP3/ufahIG137Bp7YdSAuYokVcvdviUME2eAsHju1vUz1lTsuEfxyqY8/oxZtkOUCSJt6EqT2FfTSlczPo3m4cJ/B+kn+Mid98xHV2wwieb3RsoUwNlBJ+iUdaLTNe9K2otyBQH/j2mozbkX4s4GuFneeQ1WseHDRWLqumt8muR2oR76ZK0FJcirjTX6rUzgBLShsdeR4zXChwIDAQAB";
 }
--- app/src/main/java/com/c3/www/inapp/app/MainActivity.java
+++ app/src/main/java/com/c3/www/inapp/app/MainActivity.java
@@ -1,33 +1,23 @@
 package com.c3.www.inapp.app;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
 import com.c3.www.inapp.R;
 import com.c3.www.inapp.util.IabHelper;
 import com.c3.www.inapp.util.IabResult;
 import com.c3.www.inapp.util.Inventory;
-import com.c3.www.inapp.util.Purchase;
 
-import butterknife.ButterKnife;
-import butterknife.InjectView;
-import butterknife.OnClick;
+import java.util.ArrayList;
+import java.util.List;
 
-import static com.c3.www.inapp.app.Constant.ITEM_SKU;
-import static com.c3.www.inapp.app.Constant.RC_REQUEST;
-import static com.c3.www.inapp.app.Constant.TAG;
-import static com.c3.www.inapp.app.Constant.base64EncodedPublicKey;
+import static com.c3.www.inapp.app.Constant.*;
 
 public class MainActivity extends AppCompatActivity {
-
-
-    @InjectView(R.id.buyButton)
-    Button buyButton;
 
     IabHelper mHelper;
 
@@ -35,78 +25,54 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.inject(this);
-        createPurchaseHelper();
-    }
 
-    private void createPurchaseHelper() {
+        List<String> listSKU = new ArrayList<>();
+        listSKU.add(GAS);
+        listSKU.add(INFINITE);
+        listSKU.add(PREMIUM);
+
         mHelper = new IabHelper(this, base64EncodedPublicKey);
-        mHelper.enableDebugLogging(true);
         mHelper.startSetup(new IabHelper.OnIabSetupFinishedListener() {
             @Override
             public void onIabSetupFinished(IabResult result) {
-                if (!result.isSuccess()) {
-                    Log.v("ERROR: SEt BILLING", "" + result);
-                }
-                if (mHelper == null) return;
-                mHelper.queryInventoryAsync(mGotInventoryListener);
+                if (!result.isSuccess())
+                    alert("Not Success. Error: " + result);
+
             }
         });
+
+
+
+        mHelper.queryInventoryAsync(true, listSKU, queryInventoryFinishedListener);
     }
 
-    @OnClick(R.id.buyButton)
-    void buy() {
-        mHelper.launchPurchaseFlow(this, ITEM_SKU, RC_REQUEST, mPurchase, "");
-    }
-
-    IabHelper.OnIabPurchaseFinishedListener mPurchase = new IabHelper.OnIabPurchaseFinishedListener() {
-        @Override
-        public void onIabPurchaseFinished(IabResult result, Purchase info) {
-            if (mHelper == null) return;
-            if (result.isFailure()) {
-                Log.v("FAIL", "" + result);
-                return;
-            }
-
-            Log.v(TAG, "PURCHASE SUCCESS");
-            if (info != null && verify(info)) {
-                updateUI(ITEM_SKU);
-            }
-        }
-    };
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (mHelper == null) return;
-        if (mHelper.handleActivityResult(requestCode, resultCode, data)) {
-            Log.d(TAG, "onAtivity Reuslt");
-        } else {
-            super.onActivityResult(requestCode, resultCode, data);
-        }
-    }
-
-    IabHelper.QueryInventoryFinishedListener mGotInventoryListener = new IabHelper.QueryInventoryFinishedListener() {
+    IabHelper.QueryInventoryFinishedListener queryInventoryFinishedListener = new IabHelper.QueryInventoryFinishedListener() {
         @Override
         public void onQueryInventoryFinished(IabResult result, Inventory inv) {
-            if (mHelper == null) return;
-            if (result.isFailure()) {
-                Log.v("FAILED", "" + result);
+            if (result.isFailure())
                 return;
-            }
-            Log.d(TAG, "QUEST INVO WAS SUCCESS");
-            Purchase issuePurchase = inv.getPurchase(ITEM_SKU);
-            if (issuePurchase != null && verify(issuePurchase)) {
-                updateUI(ITEM_SKU);
-            }
+            String gasPrice = inv.getSkuDetails(GAS).getPrice();
+            String infinitePrice = inv.getSkuDetails(INFINITE).getPrice();
+            String premiumPrice = inv.getSkuDetails(PREMIUM).getPrice();
+            Log.v("PRICES", gasPrice);
+            Log.v("PRICES", infinitePrice);
+            Log.v("PRICES", premiumPrice);
         }
     };
 
-    private void updateUI(String itemSku) {
-        Toast.makeText(this, "DONE", Toast.LENGTH_SHORT).show();
+    private void alert(String s) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(s);
+        builder.show();
     }
 
-    private boolean verify(Purchase issuePurchase) {
-        String payload = issuePurchase.getDeveloperPayload();
-        return true;
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (mHelper != null) mHelper.dispose();
+        mHelper = null;
     }
+
+
 }
