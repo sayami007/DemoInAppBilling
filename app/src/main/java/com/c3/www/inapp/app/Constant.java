816f7533ab0bd0abfcf6769561274342c7fe4fea
Bibesh Manandhar <bibesh.manandhar@gmail.com>
2017-07-26 14:02:08 +0545

    FROM FINAL BRANCH

--- app/src/main/java/com/c3/www/inapp/app/Constant.java
+++ app/src/main/java/com/c3/www/inapp/app/Constant.java
@@ -10,6 +10,5 @@ public class Constant {
     static final String INFINITE = "infinite_gas";
     static final String PREMIUM = "premiums";
     static final int RC_REQUEST = 10001;
-    static final String ITEM_SKU = "android.test.purchased";
     static final String base64EncodedPublicKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAtTG+2TM4viC8U3WBe18YKKy3kRkosWwg1YRzvQ+El0IzyYIzyqaGpC4WmyTU0F/1RQRWx40yosZicty+W/aL6eBfjq64JKWWMLPq+yeCPdRRUcmP+nSavPVL+macLkPEvoHNd2eCvP3/ufahIG137Bp7YdSAuYokVcvdviUME2eAsHju1vUz1lTsuEfxyqY8/oxZtkOUCSJt6EqT2FfTSlczPo3m4cJ/B+kn+Mid98xHV2wwieb3RsoUwNlBJ+iUdaLTNe9K2otyBQH/j2mozbkX4s4GuFneeQ1WseHDRWLqumt8muR2oR76ZK0FJcirjTX6rUzgBLShsdeR4zXChwIDAQAB";
 }
--- app/src/main/java/com/c3/www/inapp/app/MainActivity.java
+++ app/src/main/java/com/c3/www/inapp/app/MainActivity.java
@@ -6,27 +6,35 @@ import android.support.v4.app.Fragment;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.Button;
 
 import com.c3.www.inapp.R;
 import com.c3.www.inapp.util.IabHelper;
 import com.c3.www.inapp.util.IabResult;
 import com.c3.www.inapp.util.Inventory;
+import com.c3.www.inapp.util.Purchase;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 import static com.c3.www.inapp.app.Constant.*;
 
 public class MainActivity extends AppCompatActivity {
 
     IabHelper mHelper;
+    List<String> listSKU;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.inject(this);
 
-        List<String> listSKU = new ArrayList<>();
+        listSKU = new ArrayList<>();
         listSKU.add(GAS);
         listSKU.add(INFINITE);
         listSKU.add(PREMIUM);
@@ -37,14 +45,27 @@ public class MainActivity extends AppCompatActivity {
             public void onIabSetupFinished(IabResult result) {
                 if (!result.isSuccess())
                     alert("Not Success. Error: " + result);
-
+                mHelper.queryInventoryAsync(true, listSKU, queryInventoryFinishedListener);
             }
         });
-
-
-
-        mHelper.queryInventoryAsync(true, listSKU, queryInventoryFinishedListener);
     }
+
+    @OnClick(R.id.buyButton)
+    void onBuyButton() {
+        mHelper.launchPurchaseFlow(this,
+                GAS, RC_REQUEST, mPurchasedListener, "");
+    }
+
+    IabHelper.OnIabPurchaseFinishedListener mPurchasedListener = new IabHelper.OnIabPurchaseFinishedListener() {
+        @Override
+        public void onIabPurchaseFinished(IabResult result, Purchase info) {
+            if (!result.isSuccess())
+                Log.v(TAG, "Unsuccessful");
+            else if (info.getSku().equals(GAS)) {
+                Log.v(TAG, "Gas Purchased");
+            }
+        }
+    };
 
     IabHelper.QueryInventoryFinishedListener queryInventoryFinishedListener = new IabHelper.QueryInventoryFinishedListener() {
         @Override
@@ -60,12 +81,14 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    /**
+     * @Param Message
+     */
     private void alert(String s) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setMessage(s);
         builder.show();
     }
-
 
     @Override
     protected void onDestroy() {
